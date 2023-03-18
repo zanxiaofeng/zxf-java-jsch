ddeb0ad7d56db25b71915334fd8ab318809c74c5
davis <zanxiaofeng@163.com>
2023-03-18 18:54:26 +0800

    update

--- src/main/java/zxf/jsch/ExecTests.java
+++ src/main/java/zxf/jsch/ExecTests.java
@@ -1,4 +1,37 @@
 package zxf.jsch;
 
+import com.jcraft.jsch.*;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+
 public class ExecTests {
+    public static void main(String[] args) throws JSchException, IOException, SftpException, InterruptedException {
+        testUsernameAndPassword();
+        testUsernameAndIdentity();
+    }
+
+    private static void testUsernameAndPassword() throws JSchException, IOException, SftpException, InterruptedException {
+        System.out.println("*****************************testUsernameAndPassword*****************************");
+        Session session = JschFactory.createSession("sftp-user", "passwd", "localhost", 2222);
+        ChannelExec channelExec = (ChannelExec) session.openChannel("exec");
+        channelExec.setOutputStream(System.out, true);
+        channelExec.setCommand("cat /proc/version");
+        channelExec.connect();
+        Thread.sleep(12000);
+        channelExec.disconnect();
+        session.disconnect();
+    }
+
+    private static void testUsernameAndIdentity() throws JSchException, IOException, SftpException, InterruptedException {
+        System.out.println("*****************************testUsernameAndIdentity*****************************");
+        Session session = JschFactory.createSession("sftp-user", Paths.get("./src/main/resources/keystore/my-sshkey"), "localhost", 2222);
+        ChannelExec channelExec = (ChannelExec) session.openChannel("exec");
+        channelExec.setOutputStream(System.out, true);
+        channelExec.setCommand("cat /proc/version");
+        channelExec.connect();
+        Thread.sleep(12000);
+        channelExec.disconnect();
+        session.disconnect();
+    }
 }
