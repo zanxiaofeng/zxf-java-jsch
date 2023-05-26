5a983e5e9a654d6ead6dd0a2fd06d8571a3eb5f6
davis <zanxiaofeng@163.com>
2023-05-26 20:28:38 +0800

    update

--- src/main/java/zxf/jsch/mysftp/MySftpTests.java
+++ src/main/java/zxf/jsch/mysftp/MySftpTests.java
@@ -1,5 +1,7 @@
 package zxf.jsch.mysftp;
 
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.Logger;
 import org.apache.commons.pool2.ObjectPool;
 
 import java.nio.file.Paths;
@@ -7,6 +9,18 @@ import java.nio.file.Paths;
 public class MySftpTests {
     public static void main(String[] args) throws Exception {
         ObjectPool<MySftp> mySftpPool = MySftpPoolFactory.createPool("sftp-user", Paths.get("./src/main/resources/keystore/my-sshkey"), "localhost", 2222);
+        JSch.setLogger(new Logger() {
+            @Override
+            public boolean isEnabled(int i) {
+                return true;
+            }
+
+            @Override
+            public void log(int i, String s) {
+                System.out.println("jsch:: " + s);
+            }
+        });
+
         MySftp mySftp1 = mySftpPool.borrowObject();
         try {
             mySftp1.ls("/upload").forEach(System.out::println);
